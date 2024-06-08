8e29d6295cd7ad4cf457bd2a06b032ff3e086a75
bappy392 <bappy392@gmail.com>
2024-06-08 22:02:20 +0600

    first commit

---
+++ DOMContentLoaded_Event/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log('Event fully loaded');
+    document.getElementById("change_color").style.color='red';
+});
---
+++ nodejs_Initiate/script.js
@@ -0,0 +1,8 @@
+
+/*
+ Node Js Project Initiate
+check node js version using command: node -v
+now run script using this command: node script.js
+*/
+
+console.log('Hello js');
